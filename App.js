319b9f6968f641a58a903d3e70fcb18eac230356
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2023-10-31 11:19:27 +0530

    Next method for exporting the app

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Assalaamu alaikkum, Amhar!!!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+const Cat = () => {
   return (
     <View style={styles.container}>
-      <Text>Assalaamu alaikkum, Amhar!!!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello, I am your cat!</Text>
     </View>
   );
-}
+};
+
+export default Cat;
 
 const styles = StyleSheet.create({
   container: {
